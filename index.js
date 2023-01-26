19376fea20b1a0b01cee8010fa06511a29744333
zhalok <zhalok>
2023-01-26 22:20:42 +0600

    database connected

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config({ path: ".env" });
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((cons) => {
+    // console.log("MongoDb connected successfully Link:");
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log("Database connection unsuccessful!", err);
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(`listening to port no ${process.env.PORT}`);
